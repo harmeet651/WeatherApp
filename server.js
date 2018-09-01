1fc12b00957a516e6026623b404be25f5c802e41
harmeet651 <32997386+harmeet651@users.noreply.github.com>
2018-09-01 16:31:05 -0700

    Add existing file

---
+++ server.js
@@ -0,0 +1,121 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var request = require('request');
+//init app
+var app = express();
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//set static path
+app.use(express.static(path.join(__dirname, 'public')))
+
+//home route
+app.get('/', function(req, res){
+  res.render('public');
+});
+
+app.get("/allData", function(req, res){
+	console.log(req.url);
+	console.log(req.query);
+	var temp = req.query;
+	var data = temp.newData;
+	
+	urlNp="http://api.openweathermap.org/data/2.5/weather?lat="+req.query.lat+"&lon="+req.query.lon+"&appid=ba6ef401f6d70d1c7f50509ad91be1d0";
+	
+	var urlNew=urlNp.replace(/ /g,'+');
+	console.log(urlNp);
+	var urlNew=encodeURI(urlNp);
+	request.get({
+	    url: urlNew,
+	    json: true,
+	    headers: {'User-Agent': 'request'}
+	  }, (err, response, data) => {
+	    if (err) 
+	    {
+	    	console.log('Error:', err);
+	    }
+	     else if (res.statusCode !== 200) {
+	    	console.log('Status:', response.statusCode);
+	    } else 
+	    {
+	      
+	      	 console.log(data);
+	      	res.setHeader('content-type', 'application/json');
+			res.json(data);
+	    }
+	});    
+
+});
+
+app.get("/apiTwo", function(req, res){
+	console.log(req.url);
+	console.log(req.query);
+	var temp = req.query;
+	var data = temp.newData;
+	
+	urlNp2 = "http://api.openweathermap.org/data/2.5/weather?q="+req.query.name+"&appid=ba6ef401f6d70d1c7f50509ad91be1d0";
+
+	var urlNew2=urlNp2.replace(/ /g,'+');
+	console.log(urlNp2);
+	var urlNew2=encodeURI(urlNp2);
+	request.get({
+	    url: urlNew2,
+	    json: true,
+	    headers: {'User-Agent': 'request'}
+	  }, (err, response, data) => {
+	    if (err) 
+	    {
+	    	console.log('Error:', err);
+	    }
+	     else if (res.statusCode !== 200) {
+	    	console.log('Status:', response.statusCode);
+	    } else 
+	    {
+	      
+	      	 console.log(data);
+	      	res.setHeader('content-type', 'application/json');
+			res.json(data);
+	    }
+	});    
+
+});
+app.get("/fullWeath", function(req, res){
+	console.log(req.url);
+	console.log(req.query);
+	var temp = req.query;
+	var data = temp.newData;
+	
+	nasaApi = "http://api.openweathermap.org/data/2.5/forecast?lat="+req.query.lat+"&lon="+req.query.lon+"&appid=ba6ef401f6d70d1c7f50509ad91be1d0";
+	
+	var urlNew3=nasaApi.replace(/ /g,'+');
+	console.log(nasaApi);
+	var urlNew3=encodeURI(nasaApi);
+	request.get({
+	    url: urlNew3,
+	    json: true,
+	    headers: {'User-Agent': 'request'}
+	  }, (err, response, data) => {
+	    if (err) 
+	    {
+	    	console.log('Error:', err);
+	    }
+	     else if (res.statusCode !== 200) {
+	    	console.log('Status:', response.statusCode);
+	    } else 
+	    {
+	      
+	      	 console.log(data);
+	      	res.setHeader('content-type', 'application/json');
+			res.json(data);
+	    }
+	});    
+
+});
+
+//start server
+app.listen(3000, function(){
+  console.log('Server started on port 3000..')
+});
